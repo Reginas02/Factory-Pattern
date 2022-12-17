eca72a47312128384620fff07f4885f44d6ceb8a
Regina Naslund <rnaslund66@gmail.com>
2022-12-16 16:59:08 -0800

    FactoryPattern

---
+++ FactoryPattern/ATV.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class ATV : IVehicle
+
+    {
+        public ATV()
+        {
+        }
+        public void Drive()
+        {
+            Console.WriteLine("The ATV is driving, off road!");
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
